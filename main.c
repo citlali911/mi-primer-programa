5fb187df635703e3079e1f94ad448121358ca85e
acossio0perro <acossio0@ucol.mx>
2026-09-04 09:30:43 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("=====================\n ");
+    printf("!Hola git hub¡ \n ");
+    printf("====================\n ");
+    printf("Mi nombre es:Citlali flores\n ");
+    printf("Estoy aprendiendo programacion\n ");
+    printf("Este es mi primer programa\n ");
+    printf("publicado en git hub\n ");
+    printf("Bienvenido a mi repositorio\n ");
+    return 0;
+}
